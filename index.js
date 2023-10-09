a817b99cf6b8b28502d59dacfb29ac96bb2e3960
Yuragranovsky <yuragranovsky@gmail.com>
2023-10-09 13:25:58 +0300

    cluster solution

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 8181;
 const server = app.listen(port);
-server.on('listening', () => console.log(`server is listening on port ${port}`));
+server.on('listening', () => console.log(`server is listening on port ${port}; process id: ${process.pid}`));
 const dbConnection = new MongoConnection(`mongodb+srv://root:${process.env.MONGO_PASSWORD}@cluster0.uovkc8l.mongodb.net/college?retryWrites=true&w=majority`, 'college');
 const studentsCollection = dbConnection.getCollection("students")
 app.get('/performance/total', (req, res) => {
@@ -13,10 +13,10 @@ app.get('/performance/total', (req, res) => {
     for (let i = 0; i < count; i++) {
         total++;
     };
-    res.send({api: 'node', total, time: new Date().getTime() - startTime.getTime()});
+    res.send({pid: process.pid, api: 'node', total, time: new Date().getTime() - startTime.getTime()});
 });
 app.get('/performance/students', async (req, res) => {
     const startTime = new Date();
     const students = await studentsCollection.find({}).toArray();
-    res.send({api: 'node', total: students.length, time: new Date().getTime() - startTime.getTime()});
+    res.send({pid: process.pid, api: 'node', total: students.length, time: new Date().getTime() - startTime.getTime()});
 })
